8c20cdc515b3b4928f4be7ac3b1df218959b397e
dengzhu-hub <3052113768@qq.com>
2024-05-17 15:16:07 +0800

    add prettier-eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,18 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
+    'no-undef': 'error', // 确保启用了 no-undef 规则
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
